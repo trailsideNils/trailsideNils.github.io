0075e3138a0010d1c4a832061cdb9a283b59cd75
Trailside Nils <55211067+trailsideNils@users.noreply.github.com>
2022-04-23 12:10:41 -0500

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -2,29 +2,24 @@
 
 function check_github()
 {
-    command git --version
-    if [ $? -ne 0 ]
-    #github not installed
+#detecting git
+    command git --version || read -p 'You do not have Github Terminal installed, but this program requires it to function. Would you like to do so now? [y/n] ' INSTALLCONFIRM
+
+#installing git cli
+    if [ $INSTALLCONFIRM = "y" ]
     then
-        read -p 'You do not have Github Terminal installed, but this program requires it to function. Would you like to do so now? [y/n] ' INSTALLCONFIRM
-        
-#installing github
-        if [ $INSTALLCONFIRM = "y" ]
-        then
-            command curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-            echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-            command sudo apt update
-            command sudo apt install gh
-        fi
-    #else github is installed
+       command curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        command sudo apt update
+        command sudo apt install gh
     fi
 }
 
 function setup()
 {
-    echo "Attempting to install utility libQRencode..."
-    sudo apt-get install qrencode 
-
+#grabbing qr encoder
+    echo 'Attempting to install utility libQRencode...'
+    command qrencode -V || sudo apt-get install qrencode
 #logging in if not already
     gh auth status || gh auth login
     read -p "Please enter your Github username: " USERNAME
@@ -37,7 +32,7 @@ function setup()
     git add . && git commit -m "initial commit"
 #last bit of repo setup
     gh repo create qr-repo --public --push -r origin -s .
-    echo "The script ./qr.sh is now capable of pushing files to git and generating QR codes that can easily be sent in the files' stead."
+    echo "The script ./qr.sh is now capable of pushing files to git and generating QR codes that can easily be shared in the files' stead."
 
 }
 
